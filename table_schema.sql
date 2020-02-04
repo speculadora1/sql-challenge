2c0a6f0a0600573ccf66971886cfd6038c6b7d80
speculadora <spencerharrison0@gmail.com>
2020-02-03 22:41:01 -0500

    Added query for table creation

---
+++ table_schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE Departments (
+    dept_no VARCHAR(30) NOT NULL,
+    dept_name VARCHAR(30),
+    PRIMARY KEY(dept_no)
+);
+
+CREATE TABLE Employees (
+    emp_no INT NOT NULL,
+    birth_date DATE,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    gender VARCHAR(10),
+    hire_date DATE,
+    PRIMARY KEY(emp_no)
+);
+
+CREATE TABLE Dept_Manager (
+    dept_no VARCHAR(30) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE,
+    to_date DATE,
+    FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
+
+CREATE TABLE Dept_Emp (
+    dept_no VARCHAR(30) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE,
+    to_date DATE,
+    FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
+
+CREATE TABLE Titles (
+    emp_no INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    from_date DATE,
+    to_date DATE,
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
+
+CREATE TABLE Salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    from_date DATE,
+    to_date DATE,
+    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);

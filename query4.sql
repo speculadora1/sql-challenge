428a33c5f84fe5838dede98f902c2fc5c66749d9
speculadora <spencerharrison0@gmail.com>
2020-02-03 23:09:00 -0500

    Added query for question #4

---
+++ query4.sql
@@ -0,0 +1,10 @@
+SELECT
+    e.emp_no as "Employee Number",
+    e.last_name as "Last Name",
+    e.first_name as "First Name",
+    d.dept_name as "Department Name"
+FROM dept_emp de
+INNER JOIN employees e
+    ON de.emp_no = e.emp_no
+INNER JOIN departments d
+    ON de.dept_no = d.dept_no
